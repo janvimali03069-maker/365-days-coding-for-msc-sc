3df6579f412db3c9d3c30b061697d851818a83c9
Janvai_mali <janvimali03069@gmail.com>
2026-08-17 20:54:36 +0530

    Implement number guessing game in Dy-17.c
    Fix logic errors and improve random number generation.

---
+++ Dy-17.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+  int rN=(rand() % 100)+1;
+for(;;)
+{
+int n=0; pf/sf;
+if(n>rN)
+  printf("Higher");
+else if(n<rN)
+  printf("lower");
+else
+{
+printf("Success"); 
+     break;
+}
+return 0;
+}
+}
+    
