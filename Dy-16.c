5d6e120e2ab4a9b5c501b21839df95f448ff0cf6
Janvai_mali <janvimali03069@gmail.com>
2026-08-17 20:36:00 +0530

    Implement prime number checking in Dy-16.c
    Fix syntax errors and implement prime checking logic.

---
+++ Dy-16.c
@@ -0,0 +1,25 @@
+#include<stdioi.h>
+int main()
+{
+  int p=0; pf/sf
+    boolean flag=true;
+for(int i=2; i<p; i++)
+{
+if(p% i == 0)
+{
+flag=false;
+break;
+}
+}
+if(flag==true)
+{
+printf("P is prime");
+}
+else
+{
+printf("P is not prime");
+}
+  return o;
+}
+
+ 
